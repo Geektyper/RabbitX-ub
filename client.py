88570dd9f92d4e7d4a523fc8dfba0131f202a6e7
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 14:45:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -5,3 +5,6 @@ if TOKENS.BOT_TOKEN:
     BOT = Client(":BOT:", api_id=API.API_ID, api_hash=API.API_HASH, bot_token=TOKENS.BOT_TOKEN)
 else:
     BOT = None
+
+if TOKENS.STRING_SESSION:
+    bunny = Client(":bunny", api_id=API.API_ID, api_hash=API.API_HASH, session_string=TOKENS.STRING_SESSION)

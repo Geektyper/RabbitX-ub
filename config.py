ee3ea7c1ebd725148e8e07a2d24ffa23e9e9863e
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 09:39:36 +0530

    Update

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PMPERMIT = getenv("PMPERMIT","ENABLE")
+
+class API:
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.getenv("API_HASH", "")
+
+class TOKENS:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    STRING_SESSION = os.getenv("STRING_SESSION", "")
+    STRING_SESSION_2 = os.getenv("STRING_SESSION_2", "")
+    STRING_SESSION_3 = os.getenv("STRING_SESSION_3", "")
+    STRING_SESSION_4 = os.getenv("STRING_SESSION_4", "")
+    STRING_SESSION_5 = os.getenv("STRING_SESSION_5", "")
+    STRING_SESSION_6 = os.getenv("STRING_SESSION_6", "")
+    STRING_SESSION_7 = os.getenv("STRING_SESSION_7", "")
+    STRING_SESSION_8 = os.getenv("STRING_SESSION_8", "")
+    STRING_SESSION_9 = os.getenv("STRING_SESSION_9", "")
+    STRING_SESSION_10 = os.getenv("STRING_SESSION_10", "")
+
+class config:
+    MONGO_DATABASE = os.getenv("MONGO_DATABASE","")
+class DATABASE:
+    MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
+
+class DEV:
+    OWNER_ID = int(os.getenv("OWNER_ID", ""))
+
+    # DONT EDIT THIS 
+    SUDO_USERS = [] 
+    # YOU CAN ADD SUDO USING /addsudo
+
+class STUFF:
+    PMPERMIT = os.getenv("PMPERMIT", None)
+    ALIVE_PIC = os.getenv("ALIVE_PIC", "")
+    HELP_PIC = os. getenv("HELP_PIC", "")
+    START_PIC = os. getenv("START_PIC", "")
+    COMMAND_HANDLER = os. getenv("COMMAND_HANDLER", "!")
+    ALLOW_PORN = os.getenv("ALLOW_PORN", True) # CHANGE 'True' TO 'False' IF YOU WANNA DISABLE PORN

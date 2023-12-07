8c6a8cc53a3db931e631a888b894a0939577db87
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 15:02:12 +0530

    Update bunny

---
+++ bunny/chance.py
@@ -0,0 +1,10 @@
+from pyrogram import Client, filters
+import random
+from . import hl
+from client import bunny as Client
+
+
+@Client.on_message(filters.command("chance",  hl) & filters.me)
+async def chance(client, message):
+    text = message.text.split(hl + "chance ", maxsplit=1)[1]
+    await message.edit(f"{text}\n\n**__๏ Chance »__** {random.randint(1, 100)}%")

c2b9cd2c6844d639c86bd942edfe3d91cc414fe5
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 10:51:52 +0530

    Update bunny

---
+++ bunny/p-spam.py
@@ -0,0 +1,24 @@
+from .verify import verify
+from .data import PORMS
+import random
+from config import STUFF
+from pyrogram import Client, filters
+
+hl = STUFF.COMMAND_HANDLER
+
+@Client.on_message(filters.command(["pornspam", "ps"], hl))
+async def ps(_, m):
+  if not await verify(m.from_user.id):
+    return
+  if not STUFF.ALLOW_PORN:
+    return await m.reply("Porn spam is disabled ! For enabling, set ALLOW_PORN to True in config !")
+  try:
+    count = int(m.text.split()[1])
+  except:
+    return await m.reply(f"{hl}pornspam | {hl}ps [count]")
+  for i in range(0, count):
+    med = random.choice(PORMS)
+    if med[-3:] == "mp4":
+        await _.send_animation(m.chat.id, med)
+    else:
+        await _.send_photo(m.chat.id, med)

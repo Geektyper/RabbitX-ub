264de64967711bac5e24a5a3cc473962085c177a
Pratham <74235427+Geektyper@users.noreply.github.com>
2023-12-07 12:34:45 +0530

    Update utils

---
+++ bunny/utils/verify.py
@@ -0,0 +1,10 @@
+from bunny.database.sudo import is_sudo
+
+async def verify(_, m):
+    x = m.from_user.is_self
+    if not x:
+        id = m.from_user.id
+        sudo = await is_sudo(id)
+        if not sudo:
+            return False
+    return True
